a6ae8208e82188cbd762b5f2c44adca2cb927950
k-lia <k.liapoulos@gmail.com>
2019-06-19 09:58:31 -0500

    login.js

--- assets/javascript/definition.js
+++ assets/javascript/definition.js
@@ -1,122 +1,130 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDndDgQIX4a7GKRC0ajkUSl1CiRO_BRcD4",
+  authDomain: "project-test-56b7d.firebaseapp.com",
+  databaseURL: "https://project-test-56b7d.firebaseio.com",
+  projectId: "project-test-56b7d",
+  storageBucket: "project-test-56b7d.appspot.com",
+  messagingSenderId: "737064505924",
+  appId: "1:737064505924:web:3a8badefacc30720"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-// alert("worked"); 
+var database = firebase.database();
 
-$(document).ready(function(){
-    $('.wiki-text').each(function() {
-        var $this = $(this);
-        $this.html($this.text().replace(/\b(\w+)\b/g, "<span>$1</span>"));
-        // $this.addClass(".wiki-text");
-        // alert("wiki body");
+var language = 0;
+var apiKey = "91bf7c64-058e-48ed-80fc-ccfba37495fc";
+
+$(document).ready(function() {
+  $(".wiki-text").each(function() {
+    var $this = $(this);
+    $this.html($this.text().replace(/\b(\w+)\b/g, "<span>$1</span>"));
+    // $this.addClass(".wiki-text");
+    // alert("wiki body");
+
+    // for a clicked word, get the current user and add it to the user's saved words list
+    $this.on("click", function(e) {
+      // alert("clicked");
     });
+  });
+
+  $("div span").hover(
+    function() {
+      $(this).css("background-color", "#6dd8ff");
+      // alert(getTheMeaningOfTheWord($(this).html()));
+      getTheMeaningOfTheWord($(this).html());
+      // alert("wiki-text");
+    },
+    function() {
+      $(this).css("background-color", "");
+    }
+  );
+
+
+//   dictionary buttons
+
+$("#btnEng").on("click", function() {
+    console.log("You Clicked English");
+});
+
+$("#btnGrm").on("click", function(){
+    console.log("You Clicked German");
     
-    $('div span').hover(
-        function() { 
-            $(this).css('background-color','#6dd8ff'); 
-            // alert(getTheMeaningOfTheWord($(this).html()));
-            getTheMeaningOfTheWord($(this).html());
-            // alert("wiki-text");
-        },
-        function() { 
-            $(this).css('background-color',''); 
-        }
-    );
+});
 
-
-})
-
-var apiKey = '91bf7c64-058e-48ed-80fc-ccfba37495fc';
-
-function getTheMeaningOfTheWord(word){
-
-    var meaning = "";
-    $.ajax({
-        url: "https://www.dictionaryapi.com/api/v3/references/collegiate/json/"+word+"?key="+apiKey,
-        // url: 'http://www.dictionaryapi.com/api/v1/references/collegiate/json/'+word+'?key='+apiKey,
-        method: "GET",
-        // dataType: "jsonp"
-        success: function(response) {
-            var results = response;
-            console.log(response);
-            // var key = '0';
-            // console.log(response.key);
-            
+$("#btnSpn").on("click", function(){
+    console.log("You Clicked Spanish");
     
-            // for (var i = 0; i < results.length; i++) {
-            //     console.log(results[i]);
-                
-            // }
-            if(typeof results[0].shortdef !== 'undefined' && !(results[0].shortdef === undefined))
-            {
-                if(results[0] === undefined){
-                    meaning = results.shortdef[0];
-                    console.log(results.shortdef[0]);
-                }
-                else{
-                    meaning = results[0].shortdef[0];
-                    console.log(results[0].shortdef[0]);
-                }
-                $(".trans-text").html("<h2>"+word+"</h2>"+meaning);
-            }
+});
+});
 
-        
-        
-        },
-        error: function() {
+function getTheMeaningOfTheWord(word) {
+  var meaning = "";
+  $.ajax({
+    url:
+      "https://www.dictionaryapi.com/api/v3/references/collegiate/json/" +
+      word +
+      "?key=" +
+      apiKey,
+    // url: 'http://www.dictionaryapi.com/api/v1/references/collegiate/json/'+word+'?key='+apiKey,
+    method: "GET",
+    // dataType: "jsonp"
+    success: function(response) {
+      var results = response;
+      console.log(response);
+      // var key = '0';
+      // console.log(response.key);
 
-            
+      // for (var i = 0; i < results.length; i++) {
+      //     console.log(results[i]);
+
+      // }
+      if (results !== undefined && typeof results !== "undefined") {
+        if (
+          typeof results[0].shortdef !== "undefined" &&
+          !(results[0].shortdef === undefined)
+        ) {
+          if (results[0] === undefined) {
+            meaning = results.shortdef[0];
+            console.log(results.shortdef[0]);
+          } else {
+            meaning = results[0].shortdef[0];
+            console.log(results[0].shortdef[0]);
+          }
+          $(".trans-text").html("<h2>" + word + "</h2>" + meaning);
         }
-        // response is an xml doc
-        // console.log(response);
-        // var xml = response;
-        // var xmlDoc = $.parseXML( xml );
-        // $xml = $( xmlDoc );
-        // var $dt = $xml.find( "dt" );
-        // console.log($dt);
-        // $.parseXML(response).find('dt').each(function(index){
-        //     var provider = $(this).find('Provider').text();
-        //     var channel = $(this).find('FeedCommonName').text();
-        //     var hd = $(this).find('FeedIsHD').text();
-        //     $('.box ul').append('<li>'+channel+'</li>');
-        // });
-        // alert(word);
-
-        // $(response).find(word+"[1]").each(function(){
-        //     alert(word);
-        //     $(this).find("def").each(function(){
-        //         $(this).find("dt").each(function(){
-        //             var name = $(this).text();
-        //             alert(name);
-        //         });
-        //     });
-        // });
-
-        // $(response).find(word+"[1]").find('def').each(function(){
-        //     alert("hello?")
-        //     $(this).children().each(function(){
-        //         var tagName = this.tagName;
-        //         var val= $(this).text();
-        //         console.log(val);
-                
-        //     })
-        // });
-        
-
-        
-    });
-    return word + ": "+ meaning;
+        else{
+            console.log("error in reading short definition");
+        }
+      }
+    },
+    error: function() {}
+  });
+  return word + ": " + meaning;
 }
 
+function saveWord(word) {
+// if user is signed in, utilize user's storage
+if(firebase.auth().currentUser){
+
+
+// otherwise, commit to session storage for Guest
+}
+else{
+    sessionStorage.clear();
+    // sessionStorage.setItem("word")
+}
+}
 //   $.ajax( {
 //     url: "https://en.wikipedia.org/w/api.php",
-//     jsonp: "callback", 
-//     dataType: 'jsonp', 
-//     data: { 
-//         action: "query", 
-//         list: "search", 
-//         srsearch: "javascript", 
-//         format: "json" 
+//     jsonp: "callback",
+//     dataType: 'jsonp',
+//     data: {
+//         action: "query",
+//         list: "search",
+//         srsearch: "javascript",
+//         format: "json"
 //     },
 //     xhrFields: { withCredentials: true },
 //     success: function(response) { alert("worked")}
 // });
-
--- assets/javascript/login.js
+++ assets/javascript/login.js
@@ -20,53 +20,210 @@ var firebaseConfig = {
   appId: "1:737064505924:web:3a8badefacc30720"
 };
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+// firebase.initializeApp(firebaseConfig);
 
 var database = firebase.database();
 
 $(document).ready(function() {
   var email = "";
   var password = "";
-  $("#login-btn").on("click", function(e) {
+  // $("#display-login-btn").css("display", "none");
+  // $("#signout-btn").css("display", "inline");
+  // user logged in?
+  var user = firebase.auth().currentUser;
+
+  // if (user) {
+  //   // User is signed in.
+  //   console.log(user + " signed in");
+  // } else {
+  //   // No user is signed in.
+  //   console.log("No user is signed in");
+  // }
+  $(".imp-btn").on("click", function(e) {
     e.preventDefault();
     // alert($("#inputEmail").val());
     // alert($("#inputPassword").val());
-    email = $("#inputEmail").val();
-    password = $("#inputPassword").val();
-    // firebase.auth().createUserWithEmailAndPassword(email, password).then(function(user) {
-    //     var user = firebase.auth().currentUser;
-    //     // alert(JSON.stringify(user)); // Optional
+    // alert("clicked");
+    // new user
+    if (this.id === "new-user-btn") {
+      // alert("new user btn");
 
-    // }, function(error) {
-    //     // Handle Errors here.
-    //     var errorCode = error.code;
-    //     var errorMessage = error.message;
-    // });
+      email = $("#setup-email").val();
+      password = $("#setup-password").val();
+      console.log(email + " email");
+      console.log(password + " password");
 
-    // firebase
-    //   .auth()
-    //   .signInWithEmailAndPassword(email, password)
-    //   .then(function() {
-    //     var user = firebase.auth().currentUser;
+      if (email.length < 4) {
+        alert("Please enter an email address.");
+        return;
+      }
+      if (password.length < 4) {
+        alert("Please enter a password.");
+        return;
+      }
 
-    //     if (user) {
-    //       // User is signed in.
-    //       function writeUserData(words) {
-    //         firebase.database().ref('users/' + user).set({
-    //             words: words
-    //           //some more user data
-    //         });
-    //       }
-    //     } else {
-    //       // No user is signed in.
-    //     }
-    //   })
-    //   .catch(function(error) {
-    //     // Handle Errors here.
-    //     var errorCode = error.code;
-    //     var errorMessage = error.message;
-    //     alert("user doesn't exist/password wrong");
-    //     // ...
-    //   });
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password)
+        .then(
+          function(user) {
+            var user = firebase.auth().currentUser;
+            // alert(JSON.stringify(user));
+            // Optional
+
+            // if(user)
+            // {
+              
+            // }
+            firebase
+              .database()
+              .ref("users/" + user.uid)
+              .set({
+                firstName: $("#first-name").val(),
+                lastName: $("#last-name").val()
+                //some more user data
+              });
+
+
+            firebase.database().ref("users/"+user.uid).on("value", function(e){
+              console.log(user.uid.firstName);
+              console.log(user.uid.lastName);
+            })
+
+
+          },
+          function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+          }
+        )
+        .catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // [START_EXCLUDE]
+          if (errorCode == "auth/weak-password") {
+            alert("The password is too weak.");
+          } else {
+            alert(errorMessage);
+          }
+          console.log(error);
+          // [END_EXCLUDE]
+        });
+    }
+    // login
+    else if (this.id === "login-btn") {
+      // alert("login btn");
+      // firebase
+      //   .auth()
+      //   .signInWithEmailAndPassword(email, password)
+      //   .then(function() {
+      //     user = firebase.auth().currentUser;
+
+      //     if (user) {
+      //       // User is signed in.
+      //       // function writeUserData(words) {
+      //       //   firebase
+      //       //     .database()
+      //       //     .ref("users/" + user)
+      //       //     .set({
+      //       //       words: words
+      //       //       //some more user data
+      //       //     });
+      //       // }
+      //       alert(user + " signed in");
+      //     } else {
+      //       // No user is signed in.
+      //     }
+      //   })
+      //   .catch(function(error) {
+      //     // Handle Errors here.
+      //     var errorCode = error.code;
+      //     var errorMessage = error.message;
+      //     // alert("user doesn't exist/password wrong");
+      //     // ...
+      //   });
+      var user = firebase.auth().currentUser;
+      if (user) {
+        $("#display-login-btn").css("display", "none");
+        $("#signout-btn").css("display", "inline");
+        console.log(firebase.auth().currentUser.email);
+        $("#welcome-tag").text("Welcome, "+user.uid.firstName);
+      }
+      email = $("#input-email").val();
+      password = $("#input-password").val();
+
+      if (email.length < 4) {
+        alert("Please enter an email address.");
+        return;
+      }
+      if (password.length < 4) {
+        alert("Please enter a password.");
+        return;
+      }
+      // Sign in with email and pass.
+      // [START authwithemail]
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(email, password)
+        .then(function() {
+          user = firebase.auth().currentUser;
+          // alert("successful sign in");
+          if(user){
+            $("#display-login-btn").css("display", "none");
+            $("#signout-btn").css("display", "inline");
+          }
+
+        })
+        .catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // [START_EXCLUDE]
+          if (errorCode === "auth/wrong-password") {
+            alert("Wrong password.");
+          } else {
+            alert(errorMessage);
+          }
+          console.log(error);
+          // document.getElementById('quickstart-sign-in').disabled = false;
+          // [END_EXCLUDE]
+        });
+      // [END authwithemail]
+    } else if (this.id === "signout-btn") {
+      if (firebase.auth().currentUser) {
+        // alert("clicked");
+        // [START signout]
+        firebase.auth().signOut();
+        // [END signout]
+        $("#signout-btn").css("display", "none");
+        $("#display-login-btn").css("display", "inline");
+      }
+    }
   });
 });
+
+window.onload = function() {
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      console.log("Anonymous user signed-in.", user);
+    } else {
+      console.log(
+        "There was no anonymous session. Creating a new anonymous user."
+      );
+      // Sign the user in anonymously since accessing Storage requires the user to be authorized.
+      firebase
+        .auth()
+        .signInAnonymously()
+        .catch(function(error) {
+          if (error.code === "auth/operation-not-allowed") {
+            window.alert(
+              "Anonymous Sign-in failed. Please make sure that you have enabled anonymous " +
+                "sign-in on your Firebase project."
+            );
+          }
+        });
+    }
+  });
+};
